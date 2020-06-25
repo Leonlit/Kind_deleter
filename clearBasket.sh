7d1d7c31bfaf243b94c7965d17657b346cc9e7f0
Leon <leonlit123@gmail.com>
2020-06-25 11:56:39 +0800

    small update. learned more about bash

--- clearBasket.sh
+++ clearBasket.sh
@@ -2,7 +2,14 @@
 
 clearBasket () {
    arr=("$@") #getting the array of parameter
-   printf "are you sure you want to delete the files:\n "$@"" 
-   
+   printf "are you sure you want to delete all the files in myWasteBasket? \t"
+   read $comfirm
+   while true; do 
+      case $comfirm in
+         [Yn]* ) rm -rfv "$basketLocation"; break;;
+         [Nn]* ) printf "Aborting operation! \n"; break;;
+         * ) printf "Please answer [Y]es or [N]o.\n";;
+      esac
+   done
 
-}
+}  
--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # $1 Getting the first parameter  variable - to see which command user want to use.
-# -i, to use the normal remove command but only mov the file to a folder called mywastebasket.
+# -i, to use the normal remove command but only mov the file to a folder called mywastebasket.
 # -c, used to clear the mywastebasket folder content (ask user for comfirmation bfr the operation)
 
 #This file is mainly used for main functions like getting console variable and handle use
@@ -8,14 +8,18 @@
 #importing important files
 . ./clearBasket.sh
 
-if [ "$1" = "-i" ]; then
-   printf "So you want to move away this file"
-   elif [ $1 =  "-c"  ]; then 
+basketLocation="~/Desktop/myWasteBasket"
+
+command="$1"
+
+if [ $command = "-i" ]; then
+   printf "So you want to move away this file \n"
+   elif [ $command =  "-c"  ]; then 
       shift
-      printf "ya so you want to clear the basket?"
-      clearBasket "$@"
-   elif [ $1 = "-ic" ] || [ $1 = "-ci" ]; then 
-      printf "So you want to delete one file permanently."
+      printf "ya so you want to clear the basket? \n"
+      clearBasket "$@" #clear the folder
+   elif [ $command = "-ic" ] || [ $command = "-ci" ]; then 
+      printf "So you want to delete one file permanently. \n"
    else 
       printf "Error occured"
 fi
