9e97cbbd8fc548e717ad31ad53da477996bdd0e0
Leon <leonlit123@gmail.com>
2020-07-04 13:19:30 +0800

    fixed the issues where directory name wont increase in number when the directory name is found in the basket

--- deleteFiles.sh
+++ deleteFiles.sh
@@ -6,21 +6,17 @@ deleteFiles() {
       tempName="${basketLocation}/"$1""
       counter=0
       
-      if [ -f "$1" ]; then
-         while [ -f "$tempName" ]; do
+      if [ -f "$1" ] || [ -d "$1" ]; then
+         while [ -f "$tempName" ] || [ -d "$tempName" ];  do
             tempName=""${basketLocation}"/"$1"("$counter")"
             let "counter++"
-         done
-      else
-         while [ -d "$tempName" ] || [ -f "$tempName" ]; do
-            tempName=""${basketLocation}"/"$1"("$counter")"
-            let "counter++"
-         done
+         done 
       fi
-         
-      if [ -d $1 ]; then
+
+      if [ -d "$1" ]; then
          mv  -v "$1" "$tempName"
       else
+         echo "$tempName"
          mv "$1" "$tempName"
       fi
       shift
